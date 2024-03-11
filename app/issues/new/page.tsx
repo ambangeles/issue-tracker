4a1c01e297f425c33cd0a574d51b304834815c24
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-03-11 15:12:11 +0800

    Add Spinner component and disable submit button during form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -25,6 +26,8 @@ const NewIssuePage = () => {
 	});
 
 	const [error, setError] = useState("");
+	const [isSubmitting, setIsSubmitting] = useState(false);
+
 	return (
 		<div className="max-w-xl">
 			{error && (
@@ -35,9 +38,11 @@ const NewIssuePage = () => {
 			<form
 				onSubmit={handleSubmit(async (data) => {
 					try {
+						setIsSubmitting(true);
 						await axios.post("/api/issues", data);
 						router.push("/issues");
 					} catch (error) {
+						setIsSubmitting(false);
 						setError(
 							"Unexpected error occurred. Please try again."
 						);
@@ -60,7 +65,7 @@ const NewIssuePage = () => {
 					)}
 				/>
 				<ErrorMessage>{errors.description?.message}</ErrorMessage>
-				<Button>Submit New Issue</Button>
+				<Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner/>}</Button>
 			</form>
 		</div>
 	);
