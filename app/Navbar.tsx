ea379dba1c1b7d7f3b15336f593e64e34660ceb7
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-03-13 17:59:42 +0800

    Add avatar and dropdown menu to Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Container, Flex } from "@radix-ui/themes";
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from "@radix-ui/themes";
 import classnames from "classnames";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -47,11 +47,32 @@ const Navbar = () => {
 					</Flex>
 					<Box>
 						{status === "authenticated" ? (
-							<Link href="/api/auth/signout">Sign out</Link>
+							// <Link href="/api/auth/signout">Sign out</Link>
+							<DropdownMenu.Root>
+								<DropdownMenu.Trigger>
+									<Avatar
+										src={session.user!.image!}
+										fallback="?"
+										size={"2"}
+										radius="full"
+										className="cursor-pointer"
+									/>
+								</DropdownMenu.Trigger>
+								<DropdownMenu.Content>
+									<DropdownMenu.Label>
+										<Text size={"2"}>{session.user!.email}</Text>
+									</DropdownMenu.Label>
+									<DropdownMenu.Item>
+										<Link href="/api/auth/signout">
+											Sign out
+										</Link>
+									</DropdownMenu.Item>
+								</DropdownMenu.Content>
+							</DropdownMenu.Root>
 						) : (
 							<Link href="/api/auth/signin">Sign in</Link>
 						)}
-					</Box>{" "}
+					</Box>
 				</Flex>
 			</Container>
 		</nav>
