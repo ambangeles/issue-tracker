3523cd728fe943c3fad67973164796ccc32786b2
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-03-14 12:55:49 +0800

    Update pagination pageSize to 12

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import Pagination from "./components/Pagination";
 
 export default function Home() {
-	return <Pagination itemCount={11} pageSize={5} currentPage={1}/>;
+	return <Pagination itemCount={11} pageSize={12} currentPage={1}/>;
 }
