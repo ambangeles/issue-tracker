9a43a1888d65b41b6b0bc3af6c66a883841eaace
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-03-15 15:20:30 +0800

    Add IssueChart component and update layout in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,48 @@ import Pagination from "./components/Pagination";
 import LatestIssue from "./LatestIssue";
 import IssueSummary from "./IssueSummary";
 import prisma from "@/prisma/client";
+import { Flex, Grid } from "@radix-ui/themes";
+import IssueChart from "./IssueChart";
 
 export default async function Home() {
 	const open = await prisma.issue.count({
 		where: {
 			status: "OPEN",
 		},
-	})
+	});
 
 	const closed = await prisma.issue.count({
 		where: {
 			status: "CLOSED",
 		},
-	})
+	});
 	const inProgress = await prisma.issue.count({
 		where: {
 			status: "IN_PROGRESS",
 		},
-	})
+	});
 
-	return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
+	return (
+		<Grid
+			columns={{
+				initial: "1",
+				md: "2",
+			}}
+			gap={"5"}
+		>
+			<Flex direction={"column"} gap={"5"}>
+				<IssueSummary
+					open={open}
+					closed={closed}
+					inProgress={inProgress}
+				/>
+				<IssueChart
+					open={open}
+					closed={closed}
+					inProgress={inProgress}
+				/>
+			</Flex>
+			<LatestIssue />
+		</Grid>
+	);
 }
