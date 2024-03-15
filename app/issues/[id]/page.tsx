48596eb7b5b9c449eec8e8c39b1428a3448753e4
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-03-15 15:31:32 +0800

    Add cache for fetching user in IssueDetailPage

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,6 +7,7 @@ import DeleteIssueButton from "./DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/api/auth/authOptions";
 import AssigneeSelect from "./AssigneeSelect";
+import { cache } from "react";
 
 interface Props {
 	params: {
@@ -14,14 +15,18 @@ interface Props {
 	};
 }
 
+const fetchUser = cache((issueId: number) =>
+	prisma.issue.findUnique({
+		where: {
+			id: issueId,
+		},
+	})
+);
+
 const IssueDetailPage = async ({ params }: Props) => {
 	const session = await getServerSession(authOptions);
 
-	const issue = await prisma.issue.findUnique({
-		where: {
-			id: parseInt(params.id),
-		},
-	});
+	const issue = await fetchUser(parseInt(params.id));
 
 	if (!issue) {
 		notFound();
@@ -52,15 +57,11 @@ const IssueDetailPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({ params }: Props) {
-	const issue = await prisma.issue.findUnique({
-		where: {
-			id: parseInt(params.id),
-		},
-	});
+	const issue = await fetchUser(parseInt(params.id));
 
 	return {
 		title: `Issue Tracker - ${issue?.title}`,
-		description: `Details for issue ${issue?.title}`
+		description: `Details for issue ${issue?.title}`,
 	};
 }
 
