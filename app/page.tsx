4e577342131ce01401f6e247951303ba8e44f26e
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-03-15 15:11:55 +0800

    Add IssueSummary component and update Home function

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
 import Image from "next/image";
 import Pagination from "./components/Pagination";
 import LatestIssue from "./LatestIssue";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home() {
-	return <LatestIssue />;
+export default async function Home() {
+	const open = await prisma.issue.count({
+		where: {
+			status: "OPEN",
+		},
+	})
+
+	const closed = await prisma.issue.count({
+		where: {
+			status: "CLOSED",
+		},
+	})
+	const inProgress = await prisma.issue.count({
+		where: {
+			status: "IN_PROGRESS",
+		},
+	})
+
+	return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
